496212e487215f69e497037c7159a19379deed3c
swarna-123 <86168609+swarna-123@users.noreply.github.com>
2022-02-26 14:49:32 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,113 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world, backgroundImg;
+var canvas, angle, tower, ground, cannon, boat;
+var balls = [];
+var boats = [];
+
+
+function preload() {
+  backgroundImg = loadImage("spaceShuttleControls.jpeg");
+  towerImage = loadImage("./assets/tower.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth - 200, windowHeight - 150);
+  engine = Engine.create();
+  world = engine.world;
+  angle = -PI / 4;
+  ground = new Ground(0, height - 1, width * 2, 1);
+ 
+  cannon = new Cannon(width / 2 + 80, height / 2 + 50, 120, 40, angle);
+
+  boat = new Boat(random(10,width), 20, 200, 200, -100);
+  
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  Engine.update(engine);
+  ground.display();
+
+  showBoats();
+
+  for (var i = 0; i < balls.length; i++) {
+    showCannonBalls(balls[i], i);
+    for (var j = 0; j < boats.length; j++) {
+      if (balls[i] !== undefined && boats[j] !== undefined) {
+        var collision = Matter.SAT.collides(balls[i].body, boats[j].body);
+        if (collision.collided) {
+          boats[j].remove(j);
+
+          Matter.World.remove(world, balls[i].body);
+          balls.splice(i, 1);
+          i--;
+          
+        }
+      } 
+    }
+  }
+
+  cannon.display();
+  
+}
+
+
+//creating the cannon ball on key press
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    var cannonBall = new CannonBall(cannon.x, cannon.y);
+    cannonBall.trajectory = [];
+    Matter.Body.setAngle(cannonBall.body, cannon.angle);
+    balls.push(cannonBall);
+  }
+}
+
+// function to show the ball.
+function showCannonBalls(ball, index) {
+  ball.display();
+  if (ball.body.position.x >= width || ball.body.position.y >= height - 50) {
+    Matter.World.remove(world, ball.body);
+    balls.splice(index, 1);
+  }
+}
+
+
+//function to show the boat
+function showBoats() {
+  if (boats.length > 0) {
+    if (
+      boats.length < 4 &&
+      boats[boats.length - 1].body.position.x < width - 300
+    ) {
+      var positions = [-130, -100, -120, -80];
+      var position = random(positions);
+      var boat = new Boat(random(10,width), 20, 100, 100, position);
+      boats.push(boat);
+    }
+
+    for (var i = 0; i < boats.length; i++) {
+      Matter.Body.setVelocity(boats[i].body, {
+        x: 0,
+        y: 0.9
+      });
+
+      boats[i].display();
+    }
+  } else {
+    var boat = new Boat(random(10,width), 20, 100, 100, -100);
+    boats.push(boat);
+  }
+}
+
+
+//releasing the cannonball on key release
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    balls[balls.length - 1].shoot();
+  }
+}
